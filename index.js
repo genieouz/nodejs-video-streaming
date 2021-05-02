0aa05c987290013bb84e20533a6d4ecce38a006e
Ousmane Sakho <genieouzog@gmail.com>
2021-05-02 21:36:23 +0000

    video streaming

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 const app = require('express')();
+const fs = require('fs');
+const promisify = require('util').promisify;
+const resolve = require("path").resolve;
 
-app.get('', (req, res) => {
-    res.send("GENIEOUZ!")
+app.get('', async (req, res) => {
+    const path = resolve('videos', "myfile.mp4");
+    // const stat = await fs.stat(path);
+    const video = fs.createReadStream(path);
+    fs.stat(path, (err, stat) => {
+
+        // Handle file not found
+        if (err !== null && err.code === 'ENOENT') {
+            res.sendStatus(404);
+        }
+    
+        const fileSize = stat.size
+        const range = req.headers.range
+    
+        if (range) {
+    
+            const parts = range.replace(/bytes=/, "").split("-");
+    
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+            
+            const chunksize = (end-start)+1;
+            const file = fs.createReadStream(path, {start, end});
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            }
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            }
+            res.writeHead(200, head);
+            fs.createReadStream(path).pipe(res);
+        }
+    });
+
+
 });
 
 app.listen(8080, () => {
